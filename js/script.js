4a203cc7f3759f5891d4678122709708fe29508e
Alessandro Ganzit <alessandroganzit@gmail.com>
2025-08-15 01:15:06 +0200

    gg

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@ const navLinks = document.getElementById('nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = lightbox.querySelector("img");
+
+  const images = document.querySelectorAll(".project img, .certificazione img");
+
+  images.forEach(img => {
+    img.style.cursor = "pointer"; 
+    img.addEventListener("click", () => {
+      lightbox.style.display = "flex";
+      lightboxImg.src = img.src;
+    });
+  });
+
+  lightbox.addEventListener("click", () => {
+    lightbox.style.display = "none";
+    lightboxImg.src = "";
+  });
+});
